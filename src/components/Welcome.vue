2c7e4f69dc96172a91ff51e24d0a8e81466ad72f
csheng690 <csheng690@163.com>
2021-05-26 17:51:34 +0800

    pages

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -4,7 +4,7 @@
     <br>
     <p>观看网络课程视频，跟做出来的项目。后端接口用的是课程给的有接口文档素材。</p>
     <br>
-    <P>前端代码：<a href="https://www.liulongbin.top:8888/api/private/v1/">https://www.liulongbin.top:8888/api/private/v1/</a></p>
+    <P>前端代码：<a href="https://csheng690.github.io/vue_manage/">https://csheng690.github.io/vue_manage/</a></p>
     <br>
     <P>后端接口地址：<a href="https://www.liulongbin.top:8888/api/private/v1/">https://www.liulongbin.top:8888/api/private/v1/</a></p>
     </div>
